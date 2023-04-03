b890204b8e25fb7a9d1239ec73100904910ec7c3
Marcos-fa <marcos.fa0216@gmail.com>
2023-04-03 10:42:32 -0700

    deleted dist

--- dist/components/StatusBarManager.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModalProps } from "react-native";
-declare const StatusBarManager: ({ presentationStyle, }: {
-    presentationStyle?: ModalProps["presentationStyle"];
-}) => null;
-export default StatusBarManager;
--- dist/components/videoIcon.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-export interface ISvgProps {
-    width: number;
-    height: number;
-    color?: string;
-}
-declare const VideoIcon: (props: ISvgProps) => JSX.Element;
-export default VideoIcon;
